2aa242ab50fbddbfa05aed306e3fbb7deeed11e8
Alexandra Poluektova <90984216+p0lex@users.noreply.github.com>
2021-10-01 18:37:08 +0300

    Задание 5

---
+++ Z5/Z5/Z5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main()
+{
+	float a = 34, b = -43, aa, ab, s, r, p, c;
+	aa = abs(a);
+	ab = abs(b);
+	s = aa + ab;
+	r = (abs)(aa - ab);
+	p = aa * ab;
+	c = aa / ab;
+	cout << s << '\n' << r << '\n' << p << '\n' << c;
+
+
+
+}
