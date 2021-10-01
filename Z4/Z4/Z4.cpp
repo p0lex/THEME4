28bc963457f75c3c38c8180f9d7773832a4a1ae2
Alexandra Poluektova <90984216+p0lex@users.noreply.github.com>
2021-10-01 18:36:46 +0300

    Задание 4

---
+++ Z4/Z4/Z4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main()
+{
+	float a = 34, b = 23, s, c, p, r;
+	s = a * a + b * b;
+	p = a * a * b * b;
+	r = (abs)(a * a - b * b);
+	c = a * a / b / b;
+	cout << s << "\n" << p << "\n" << r << "\n" << c;
+
+}
